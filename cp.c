c11f82ab8e4c4587c4675289e982acd349f189b3
tyrtoves <tyrtoves@ya.ru>
2014-03-16 21:49:01 +0300

    Update cp.c

--- cp.c
+++ cp.c
@@ -8,176 +8,94 @@
 #include <string.h>
 #include <fcntl.h>
 
-struct argum {
-	char** this_argums;
-	int count;
-};
+void cpFilesToDir(char *path_from, char *path_to ) {
+	ssize_t read_bytes;
+	char buf[500];
+	int fd1 = open( path_from, O_RDONLY );
+	int fd2 = open( path_to, O_WRONLY | O_CREAT | O_TRUNC, 0777 );
 
-void cpFilesToDir(struct argum arg) {
-	int i;
-	for (i = 2; i < arg.count - 1; ++i) {
-		int fd_1 = open(arg.this_argums[i], O_RDONLY);
-		if (fd_1 < 0) {
-			perror("File error(1)");
-			_exit(0);
-		}		
-		int fd_2 = open(arg.this_argums[arg.count - 1], O_WRONLY|O_TRUNC|O_CREAT, 0666);
-		if (fd_1 < 0) {
-			perror("File error(2)");
-			close(fd_1);
-			_exit(0);
-		}
-		int n;
-		void* buf = malloc(500);
-		if (buf == NULL) {
-			perror("Memory error");
-			close(fd_1);
-			close(fd_2);
-			_exit(0);
-		}
-		while(n = read(fd_1, buf, 100) > 0) {
-			int param = write(fd_2, buf, n);
-			if (param <= 0) {
-				perror("Write error");
-				close(fd_1);
-				close(fd_2);
-				free(buf);
-				_exit(0);
-			}
-		}
-		if (n < 0) {
-			perror("Read error");
-			close(fd_1);
-			close(fd_2);
-			free(buf);
-			_exit(0);
-		}
-		close(fd_1);
-		close(fd_2);
-		free(buf);		
+	if ( fd1 < 0 || fd2 < 0 ) 
+	{
+		perror("Open error");
+		return;
 	}
+
+	while (( read_bytes = read( fd1, &buf, 500 )) > 0 )
+	{
+		if ( write( fd2, &buf, read_bytes) < 0 )
+		{
+			perror("Writing error");
+			return;
+		}
+	}
+	
+	close(fd1);
+	close(fd2);
+	
 }
 
-void cpDirsToDir(struct argum arg) {
-	int i;
-	for (i = 3; i < arg.count - 1; ++i) {
-		char pathName[PATH_MAX + 1];
-		strncpy( pathName, arg.this_argums[arg.count - 1], PATH_MAX );
-		strncat( pathName, "/", PATH_MAX );
-       	strncat( pathName, arg.this_argums[i], PATH_MAX );
-       	DIR *dir_to;		
-       	if ( ( dir_to = opendir(pathName) ) == NULL) { //If dir does not exist
-       		if ( ( mkdir(pathName, 0777) ) < 0) {
-				perror("MK_DIR");
-			}
-			else {
-				DIR *dir;
-				dir = opendir(arg.this_argums[i]);
-				struct dirent *dir_str;
-				while ( (dir_str = readdir(dir)) != NULL) {
-					if (dir_str->d_name[0] != '.') {
-						if (dir_str->d_type & S_IFDIR) { // If dir
-							char pathNameFrom[PATH_MAX];
-							strncpy( pathNameFrom, arg.this_argums[i], PATH_MAX );
-							strncat( pathNameFrom, "/", PATH_MAX );
-		   					strncat( pathNameFrom, dir_str->d_name, PATH_MAX );	
-							char* this_argum[5] = {"", "", "", pathNameFrom, pathName};
-							struct argum temp = {.this_argums=this_argum, .count=5};
-							cpDirsToDir(temp);
-						}
-						if (dir_str->d_type & S_IFREG) { // If file
-							char pathNameFrom[PATH_MAX];
-							strncpy( pathNameFrom, arg.this_argums[i], PATH_MAX );
-							strncat( pathNameFrom, "/", PATH_MAX );
-		   					strncat( pathNameFrom, dir_str->d_name, PATH_MAX );	
-							int fd_1 = open(pathNameFrom, O_RDONLY);
-							if (fd_1 < 0) {
-								perror("File error(1)");
-								_exit(0);
-							}		
-							int fd_2 = open(pathName, O_WRONLY|O_TRUNC|O_CREAT, 0666);
-							if (fd_1 < 0) {
-								perror("File error(2)");
-								close(fd_1);
-								_exit(0);
-							}
-							int n;
-							void* buf = malloc(500);
-							if (buf == NULL) {
-								perror("Memory error");
-								close(fd_1);
-								close(fd_2);
-								_exit(0);
-							}
-							while( ( n = read(fd_1, buf, 500) ) > 0) {
-								int param = write(fd_2, buf, n);
-								if (param <= 0) {
-									perror("Write error");
-									close(fd_1);
-									close(fd_2);
-									free(buf);
-									_exit(0);
-								}
-							}
-							if (n < 0) {
-								perror("Read error");
-								close(fd_1);
-								close(fd_2);
-								free(buf);
-								_exit(0);
-							}
-							close(fd_1);
-							close(fd_2);
-							free(buf);		
-						}
-					}
-				}
-			}
-   		}
-       	else { //If dir already exists
-			DIR *dir;
-			dir = opendir(arg.this_argums[i]);
-			struct dirent *dir_str;
-			while ( (dir_str = readdir(dir)) != NULL) {
-				if (dir_str->d_type & S_IFDIR) { // If dir
-					char pathNameFrom[PATH_MAX];
-					strncpy( pathNameFrom, arg.this_argums[i], PATH_MAX );
-					strncat( pathNameFrom, "/", PATH_MAX );
-					strncat( pathNameFrom, dir_str->d_name, PATH_MAX );	
-					char* this_argum[5] = {"", "", "", pathNameFrom, pathName};
-					struct argum temp = {.this_argums=this_argum, .count=5};
-					cpDirsToDir(temp);
-				}
-				if (dir_str->d_type & S_IFREG) { // If file
-					char pathNameFrom[PATH_MAX];
-					strncpy( pathNameFrom, arg.this_argums[i], PATH_MAX );
-					strncat( pathNameFrom, "/", PATH_MAX );
-					strncat( pathNameFrom, dir_str->d_name, PATH_MAX );	
-					char* this_argum[4] = {"", "", pathNameFrom, pathName};
-					struct argum temp = {.this_argums=this_argum, .count=4};
-					cpDirsToDir(temp);
-				}
-			}				       		
-       	}
-	}
-}
+void cpDirsToDir(char *path_from, char *path_to ) {
+	
+	DIR *dir_from, *dir_to;
+	struct dirent *cur_dir;
+	struct stat inform;
+	char newPathFrom[ PATH_MAX + 1];
+	char newPathTo[ PATH_MAX + 1];
+		       
+	dir_from = opendir( path_from );
 
+	if ( !dir_from ) {
+	 	perror("PATH_FROM");
+	  	return;	
+	}
+
+	mkdir( path_to, 0777 );
+	
+	while ( ( cur_dir = readdir( dir_from ) ) != NULL ) {	
+	 	if ( cur_dir->d_name[0] == '.')
+			continue;
+		
+		strncpy( newPathFrom, path_from, PATH_MAX );
+		strncat( newPathFrom, "/", PATH_MAX );
+		strncat( newPathFrom, cur_dir -> d_name, PATH_MAX );
+	
+	   	strncpy( newPathTo, path_to, PATH_MAX);
+   		strncat( newPathTo, "/", PATH_MAX );
+	    strncat( newPathTo, cur_dir -> d_name, PATH_MAX );
+
+	    if ( stat( newPathFrom, &inform ) < 0 ){
+	    	 perror("STAT");
+		}
+
+	    if (inform.st_mode & S_IFREG)
+	    	cpFilesToDir( newPathFrom, newPathTo );
+
+		if (inform.st_mode & S_IFDIR)
+			cpDirsToDir( newPathFrom, newPathTo );		
+	}
+	closedir( dir_from );
+}
 
 int main(int argc, char** argv) {
 
-	struct argum beg = {.this_argums=argv, .count=argc};
 	
-	if (argc <= 3) {
+	if (argc < 3) {
 		char* msg = "Error: wrong number of arguments";
 		write(2, msg, strlen(msg));
 		_exit(0);
 	}
 
-	if (strcmp(argv[2], "-R") == 0) {
-		cpDirsToDir(beg);	
+	if (strcmp(argv[1], "-R") == 0) {
+		int i = 0;
+		for (i = 2; i < argc - 1; ++i) {
+			cpDirsToDir( argv[i], argv[argc - 1] );
+		}
 	}
 	else {
-		cpFilesToDir(beg);
+		int i = 0;
+		for (i = 1; i < argc - 1; ++i) {
+			cpFilesToDir( argv[i], argv[argc - 1] );
+		}
 	}
-	return(0);
+	return 0;
 }
